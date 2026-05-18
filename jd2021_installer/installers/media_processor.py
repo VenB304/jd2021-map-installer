d0d83023d89cac4102d3582d181ea58b57ed1f9d
Ven <23-62629@g.batstate-u.edu.ph>
2026-05-18 16:12:55 +0800

    Revert "Change bundled gesture template order"
    This reverts commit 3ed16b626aa5258700d7621324293e6fc0a07131.

--- jd2021_installer/installers/media_processor.py
+++ jd2021_installer/installers/media_processor.py
@@ -1376,8 +1376,8 @@ def copy_moves(
     if expected_names:
         def _pick_template_gesture() -> Optional[Path]:
             bundled_candidates = [
-                Path(__file__).resolve().parents[2] / "assets" / "gesture_templates" / "discorope.gesture",
                 Path(__file__).resolve().parents[2] / "assets" / "gesture_templates" / "durango_template.gesture",
+                Path(__file__).resolve().parents[2] / "assets" / "gesture_templates" / "discorope.gesture",
             ]
             for candidate in bundled_candidates:
                 if candidate.exists():
